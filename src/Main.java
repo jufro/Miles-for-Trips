b7bf29d5c352884b2932f77d953f1d4c185490df
Yuliya Frolova <jupunkevich@gmail.com>
2020-03-14 22:09:53 +0300

    Commit gitignore and javafile

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 1000;
+        int milePrice = 20;
+
+        int bonusMiles = ticketPrice / milePrice;
+        System.out.println(bonusMiles);
+    }
+}
